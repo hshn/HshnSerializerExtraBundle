9ce4ad88651324ce9bbc3f29270353285b146ea7
Shota Hoshino <lga0503@gmail.com>
2014-12-15 21:55:24 +0900

    Setup functional bundle

---
+++ tests/Functional/Bundle/HshnSerializerExtraTestBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hshn\SerializerExtraBundle\Functional\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Shota Hoshino <lga0503@gmail.com>
+ */
+class HshnSerializerExtraTestBundle extends Bundle
+{
+
+}
