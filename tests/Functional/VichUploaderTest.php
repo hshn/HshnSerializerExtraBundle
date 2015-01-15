ca6fef0465367887ae70ac2cddbf7b8129b3a7f4
Shota Hoshino <lga0503@gmail.com>
2015-01-15 20:48:38 +0900

    Added red test

--- tests/Functional/VichUploaderTest.php
+++ tests/Functional/VichUploaderTest.php
@@ -17,10 +17,27 @@ class VichUploaderTest extends WebTestCase
         return 'vich_uploader';
     }
 
-    public function test()
+    public function testPicture()
     {
         $client = $this->createClient();
         $client->request('GET', '/picture/show');
+
+        $response = $client->getResponse();
+        $this->assertEquals($response::HTTP_OK, $response->getStatusCode());
+
+        $content = $response->getContent();
+        $this->assertJson($content);
+
+        $content = json_decode($content, true);
+        $this->assertArrayHasKey('name', $content);
+        $this->assertArrayHasKey('file_url', $content);
+        $this->assertEquals('/images/symfony.png', $content['file_url']);
+    }
+
+    public function testPictureProxy()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/picture/show/proxy');
 
         $response = $client->getResponse();
         $this->assertEquals($response::HTTP_OK, $response->getStatusCode());
